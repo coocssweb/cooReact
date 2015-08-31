ed816358eac019d118002790f445028bfe45f475
coocssweb <1974740999@qq.com>
2015-08-31 14:19:19 +0800

    update imageCut

--- components/ImageCut/ImageCut.js
+++ components/ImageCut/ImageCut.js
@@ -1,7 +1,7 @@
 var React = require("react");
 var $ = require("jquery");
 var ImageCut = React.createClass({
-    getInitalState : function(){
+    getInitialState : function(){
         return {
             isUpdate : false
         }
@@ -9,10 +9,12 @@ var ImageCut = React.createClass({
     getDefaultProps : function(){
         return {
             isTouchdown     : false,    //是否正在按下
+            isInit          : true,     //是否是初始化
+            canvas          : null,     //画布
             imageSrc        : "",       //图片路径
             innerStyle      : {         //图片位置
                         left : "0px",
-                        top : "0px"
+                        top  : "0px"
                     },
             imageOffSet     :{          //偏移位置
                 x : 0,
@@ -29,6 +31,18 @@ var ImageCut = React.createClass({
             nowPos          :{
                 x : 0,
                 y : 0
+            },
+            image           :{
+                width  : 0,
+                height : 0,
+                x      : 0,
+                y      : 0
+            },
+            cut             :{
+                width  : 196,
+                height : 196,
+                x      : 0,
+                y      : 0
             }
         }
     },
@@ -36,24 +50,39 @@ var ImageCut = React.createClass({
         var img = new Image();
         var $that = this;
         $(img).load(function() {
-            var canvas = document.createElement("canvas");
+            $that.props.canvas = document.createElement("canvas");
 
             if(img.width > 300){
-                var wWidth = $(".cut-container").width();
-                var wHeight =$(".cut-container").height();
                 var height = img.height * (300/img.width);
-                canvas.width = 300;
-                canvas.height = height;
-                canvas.getContext('2d').drawImage(img, 0, 0, 300 ,height);
+                $that.props.canvas.width = 300;
+                $that.props.canvas.height = height;
+                $that.props.canvas.getContext('2d').drawImage(img, 0, 0, 300 ,height);
+                $that.props.image.width = 300;
+                $that.props.image.height = height;
+            }else{
+                var $image = $(".cut-image");
+                $that.props.image.width = $image.width();
+                $that.props.image.height = $image.height();
             }
-            $that.props.imageSrc = canvas.toDataURL("image/png");
+
+            var $container = $(".cut-container");
+            $that.props.image.x = ($container.width() - $that.props.image.width)/2;
+            $that.props.image.y = ($container.height() - $that.props.image.height)/2;
+            $that.props.imageSrc = $that.props.canvas.toDataURL("image/png");
+
+
             $that.setState({
-                isUpdate : true
+                isUpdate : !$that.state.isUpdate
             });
         })
         .error(function() {
         })
         .attr("src", this.props.imageSrc);
+    },
+    componentDidMount : function(){
+        var $cut = $(".overlay-inner");
+        this.props.cut.x = $cut.offset().left;
+        this.props.cut.y = $cut.offset().top;
     },
     onTouchStart : function(e){
         if(this.props.isTouchDown){
@@ -66,6 +95,7 @@ var ImageCut = React.createClass({
         var touch=window.event.touches[0]||window.event.changedTouches[0];
         this.props.startPos.x = touch.pageX;
         this.props.startPos.y = touch.pageY;
+        console.log("start "+ this.props.image.x+"        "+this.props.image.y)
     },
     onTouchMove : function(e){
         if(!this.props.isTouchDown){
@@ -75,15 +105,37 @@ var ImageCut = React.createClass({
         var touch=window.event.touches[0]||window.event.changedTouches[0];
         this.props.nowPos.x = touch.pageX;
         this.props.nowPos.y = touch.pageY;
+
+
+
         this.props.touchDelate.x = this.props.nowPos.x - this.props.startPos.x;
         this.props.touchDelate.y = this.props.nowPos.y - this.props.startPos.y;
+
+        console.log(this.props.touchDelate.x + "   " +this.props.touchDelate.y)
+
+        if(
+            (this.props.image.x  + this.props.touchDelate.x - this.props.cut.x) < 0 &&
+            (this.props.image.x + this.props.image.width + this.props.touchDelate.x - (this.props.cut.x + this.props.cut.width) ) > 0
+        ){
+            this.props.image.x += this.props.touchDelate.x;
+        }
+        if(
+            (this.props.image.y  + this.props.touchDelate.y - this.props.cut.y) < 0 &&
+            (this.props.image.y + this.props.image.height + this.props.touchDelate.y - (this.props.cut.y + this.props.cut.height) ) > 0
+        ){
+            this.props.image.y += this.props.touchDelate.y;
+        }
+        this.props.isInit = false;
+        this.setState({
+            isUpdate : !this.state.isUpdate
+        });
 
     },
     onTouchEnd : function(e){
         if(!this.props.isTouchDown){
             return;
         }
-
+        this.props.isTouchDown = false;
         this.props.startPos.x = 0;
         this.props.startPos.y = 0;
         this.props.nowPos.x = 0;
@@ -91,6 +143,15 @@ var ImageCut = React.createClass({
         this.props.touchDelate.x = 0;
         this.props.touchDelate.y = 0;
 
+    },
+    onCut : function(){
+        this.props.canvas.width = this.props.cut.width;
+        this.props.canvas.height = this.props.cut.height;
+        var left = this.props.cut.x - this.props.image.x;
+        var right = this.props.cut.y - this.props.image.y;
+        this.props.canvas.getContext('2d').drawImage($(".cut-image").get(0), left, right, this.props.cut.width, this.props.cut.height
+            , 0, 0, this.props.cut.width, this.props.cut.height);
+        window.open(this.props.canvas.toDataURL("image/png"));
     },
     render : function(){
 
@@ -100,6 +161,10 @@ var ImageCut = React.createClass({
             onTouchMove : this.onTouchMove,
             onTouchEnd : this.onTouchEnd
         }
+        var innerStyle ={
+                left : this.props.image.x,
+                top : this.props.image.y
+            }
 
         return (
             <div className="cut-container" {...Events} >
@@ -108,8 +173,8 @@ var ImageCut = React.createClass({
                         <div className = "overlay-inner"></div>
                     </div>
                 </div>
-                <img src ={this.props.imageSrc} />
-                <a href="javascript:;" className="btn btn-save">保存</a>
+                <a href="javascript:;" className="btn btn-cut" onTouchEnd={this.onCut}>保存</a>
+                <img className="cut-image" src ={this.props.imageSrc} style={innerStyle} />
             </div>
         )
     }
