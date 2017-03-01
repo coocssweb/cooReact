d3e6570337ac252ed121601409329a09f971d31a
coocssweb <1974740999@qq.com>
2017-03-01 16:22:01 +0800

    load more 修改为es6写法

--- src/CooComponents/CooLoadmore/Index.js
+++ src/CooComponents/CooLoadmore/Index.js
@@ -3,37 +3,36 @@
  * 加载更多组件
  */
 
-var React = require('react');
-var PropTypes = React.PropTypes;
-var Style = require('./index.css');
+import React, { PropTypes, Component } from 'react'
+var Style = require('./index.css')
 
-var Index = React.createClass({
-    propTypes:{
-        isShow: PropTypes.bool.isRequired,          //是否显示
-        isLoading: PropTypes.bool.isRequired,       //正在加载中？
-        style: PropTypes.object,
-        onLoad: PropTypes.func.isRequired           //加载回调
-    },
-    onLoad: function(e){
-        if(this.props.isLoading){
-            return;
+export default class index extends Component{
+    constructor(props){
+        super(props)
+        this.propTypes = {
+            isLoading: PropTypes.bool.isRequired,       //正在加载中？
+            onLoad: PropTypes.func.isRequired           //加载回调
         }
-        this.props.onLoad();
-    },
-    render: function () {
+    }
+
+    onLoad(e){
+        if(this.props.isLoading){
+            return
+        }
+        this.props.onLoad()
+    }
+
+    render () {
         return (
             <a href="javascript:;"
-               className={Style['coo-btn-loadmore']+' '+(this.props.isLoading?Style['coo-btn-loadmore-loading']:'')}
-               onClick={this.onLoad}
-               style={this.props.style}>
-                <span  className={Style['coo-icon-label']}>
+               className={`${Style['coo-btn-loadmore']} ${this.props.isLoading?Style['coo-btn-loadmore-loading']:null}`}
+               onClick={this.onLoad.bind(this)}>
+                <span className={Style['coo-icon-label']}>
                     <i className={Style['coo-icon-loadmore']} />
-                    {this.props.isLoading?'加载中':'加载更多'}
+                    {this.props.isLoading?'加载中...':'加载更多'}
                 </span>
-                <span className={Style['coo-btn-loadmore-inner']} style={this.props.style} />
+                <span className={Style['coo-btn-loadmore-inner']}  />
             </a>
         )
     }
-});
-
-module.exports = Index;
+}
